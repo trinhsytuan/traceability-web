db6002a105c9a22288d955789f271841f2307231
Tuan <trinhsytuan@gmail.com>
2024-09-06 07:59:49 +0700

    Update message

--- src/app/containers/ModalShowVerifyBlockchain/ModalShowVerifyBlockchain.js
+++ src/app/containers/ModalShowVerifyBlockchain/ModalShowVerifyBlockchain.js
@@ -44,7 +44,7 @@ function ModalShowVerifyBlockchain({ isVisible, handleCancel, linkToUrl }) {
           ...textContent,
           {
             title: 'Xác thực thành công',
-            description: 'Đây là thông tin hợp lệ và được xác thực trên nền tảng Blockchain của HDU',
+            description: 'Đây là thông tin hợp lệ và được xác thực trên nền tảng Blockchain của Đại học Hồng Đức',
             link: linkToUrl,
           },
         ];
